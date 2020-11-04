d13a33cf58dd3e503d908ed2337a6977daa8d09f
fields1631 <15218361552@163.com>
2020-11-04 14:22:03 +0800

    Update test

--- test.py
+++ test.py
@@ -4,6 +4,6 @@ from fpeg.utils import Spliter
 
 
 spliter = Spliter()
-X = np.random.rand(128, 128, 3)
-spliter.recv_send(X)
+X = np.random.rand(150, 140, 3)
+spliter.recv_send([X[:, :, 0], X[:, :, 1], X[:, :, 2]])
 print(spliter.logs, len(spliter.recieved_), len(spliter.sended_))
