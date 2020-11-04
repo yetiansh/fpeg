db88a1cff31527001027d23ebef2e1248a0f6193
fields1631 <15218361552@163.com>
2020-11-04 14:17:15 +0800

    Define Config class

--- fpeg/config.py
+++ fpeg/config.py
@@ -1,11 +1,70 @@
-import configparser
+import os
+import pickle
 
 
-config_path = r"config.ini"
-config = configparser.RawConfigParser()
-config.add_section("accelerate")
-config.set("accelerate", "codec_min_task_number", 10)
-config.set("accelerate", "codec_max_pool_size", 30)
+absdir = os.path.split(os.path.abspath(__file__))[0]
+config_path = os.path.join(absdir, "config.dat")
 
-with open(config_path, 'w') as file:
-  config.write(file)
+
+class Config:
+  def __init__(self, 
+               path=None):
+    if path is not None:
+      self.path = path
+      self.read(path)
+      print(path, self.config)
+    else:
+      self.path = None
+      self.config = {}
+
+  def get(self, section, item):
+    if section in self.config:
+      if item in self.config[section]:
+        return self.config[section][item]
+
+    return None
+
+  def set(self, section, item, value):
+    if section in self.config:
+      if item not in self.config[section]:
+        self.config[section][item] = value
+
+  def add_section(self, section):
+    if section in self.config:
+      return
+
+    self.config[section] = {}
+
+  def read(self,
+           path=None):
+    if path is None:
+      path = self.path
+
+    self.config = pickle.load(open(path, "rb"))
+
+  def write(self,
+            path=None):
+    if path is None:
+      path = self.path
+
+    pickle.dump(self.config, open(path, "wb"))
+
+
+def read_config():
+  return Config(config_path)
+
+
+if __name__ == "__main__":
+  config = Config()
+
+  config.add_section("accelerate")
+  config.set("accelerate", "codec_min_task_number", 10)
+  config.set("accelerate", "codec_max_pool_size", 30)
+
+  config.add_section("log")
+  config.set("log", "fmt", "")
+
+  config.add_section("preprocess")
+  config.set("preprocess", "tile_shape", (64, 64))
+
+  config.write(config_path)
