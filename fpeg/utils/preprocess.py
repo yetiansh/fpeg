c4dbd254808e3cd4ee12b066acc1758410114ffa
fields1631 <15218361552@163.com>
2020-11-06 09:56:20 +0800

    Modify spliter

--- fpeg/utils/preprocess.py
+++ fpeg/utils/preprocess.py
@@ -1,21 +1,20 @@
 from math import ceil
 import numpy as np
+
 from ..base import Pipe
 from ..config import read_config
 from .format import Formatter
-from .pprint import Pprinter
 from .monitor import Monitor
 
 
 config = read_config()
 
 tile_shape = config.get("preprocess", "tile_shape")
-fmt = config.get("log", "fmt")
 
 
 class Spliter(Pipe):
   """
-  Spliter splits channels of image to tiles.
+  Spliter splits each channels of image to tiles.
   """
 
   def __init__(self,
@@ -28,28 +27,19 @@ class Spliter(Pipe):
     -------------------
     name: str, optional
       Name of the spliter.
-    
-    Implicit Attributes
-    -------------------
-    logs: str
-      Log messages of recieving and sending data.
-    formatter: Formatter
-      Formatter for generating log messages.
-    pprinter: fpeg.utils.pprint.Pprinter
-      Pretty printer for printing reader.
-    monitor: fpeg.utils.monitor.Monitor
+    tile_shape: tuple of int, optional
+      Shape of tiles that spliter tries to split.
     """
+    super().__init__()
+
     self.name = name
     self.tile_shape = tile_shape
 
-    self.logs = []
-    self.formatter = Formatter(fmt=fmt)
-    self.pprinter = Pprinter()
-    self.monitor = Monitor()
-
   def recv(self, X, **params):
     self.logs.append("")
-    self.recieved_ = X
+    self.logs[-1] += self.formatter.message("Receiving data.")
+    self.received_ = X
+    self.logs[-1] += self.formatter.message("Splitting data into tiles with shape {}.".format(self.tile_shape))
     shape = X[0].shape
     row_indices = np.arange(self.tile_shape[0], shape[0], self.tile_shape[0], dtype=int)
     col_indices = np.arange(self.tile_shape[1], shape[1], self.tile_shape[1], dtype=int)
