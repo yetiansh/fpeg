c70b88b4cb1113678e7556e8c7c0ddb493637061
fields1631 <15218361552@163.com>
2020-11-12 20:50:55 +0800

    Add colo transform, normalization, etc

--- fpeg/utils/process.py
+++ fpeg/utils/process.py
@@ -10,6 +10,100 @@ tile_shape = config.get("preprocess", "tile_shape")
 depth = config.get("preprocess", "depth")
 
 
+class ColorTransformer(Pipe):
+  """
+  ColorTransformer 
+  """
+
+  def __init__(self,
+               name="Color transformer",
+               mode="transform",
+               lossy=False):
+    """
+    Init and set attributes of a color transformer.
+
+    Explicit Attributes
+    -------------------
+    name: str, ofptional
+      Name of the spliter.
+    mode: str, optional
+      Mode of spliter, must in ["transform", "reverse transform"]
+    lossy: bool, optional
+      Whether the transform is lossy or lossless.
+    """
+    super().__init__()
+    
+    self.name = name
+    self.mode = mode
+    self.lossy = lossy
+
+  def recv(self, X, **params):
+    self.logs.append("")
+    self.logs[-1] += self.formatter.message("Receiving data.")
+    self.received_ = X
+
+    if self.mode == "transform":
+      x = X[0]
+      B = x[:, :, 0]
+      G = x[:, :, 1]
+      R = x[:, :, 2]
+      if self.lossy:
+        """
+        R, G, B range from -1/2 to 1/2.
+        Y, Cb, Cr range from -1/2 to 1/2.
+        """
+        self.logs[-1] += self.formatter.message("Praticing ICT.")
+        Y = 0.299 * R + 0.587 * G + 0.114 * B
+        Cb = 0.5 * (B - Y) / (1 - 0.114)
+        Cr = 0.5 * (R - Y) / (1 - 0.114)
+        x = np.array([Y, Cb, Cr])
+      else:
+        """
+        R, G, B range from -2^(depth-1) to 2^(depth-1)-1.
+        Y ranges from -2^(depth-1) to 2^(depth-1)-1.
+        Db, Dr range from 1-2^depth to 2^depth-1.
+        """
+        self.logs[-1] += self.formatter.message("Praticing RCT.")
+        Y = np.floor((R + 2 * G + B) / 4)
+        Db = B - G
+        Dr = R - G
+        x = np.array([Y, Db, Dr], dtype=int)
+
+      x = np.swapaxes(x, 0, 1)
+      x = np.swapaxes(x, 1, 2)
+      self.sended_ = [x]
+    elif self.mode == "reverse transform":
+      x = X[0]
+      if self.lossy:
+        self.logs[-1] += self.formatter.message("Praticing IICT.")
+        Y = x[:, :, 0]
+        Cb = x[:, :, 1]
+        Cr = x[:, :, 2]
+        R = Y + 1.402 * Cr
+        B = Y + 1.772 * Cb
+        G = Y - 0.344136 * Cb - 0.714136 * Cr
+        x = np.array([B, G, R])
+      else:
+        self.logs[-1] += self.formatter.message("Praticing IRCT.")
+        Y = x[:, :, 0]
+        Db = x[:, :, 1]
+        Dr = x[:, :, 2]
+        G = Y - np.floor((Db + Dr) / 4)
+        B = Db + G
+        R = Dr + G
+        x = np.array([B, G, R], dtype=int)
+
+      x = np.swapaxes(x, 0, 1)
+      x = np.swapaxes(x, 1, 2)
+      self.sended_ = [x]
+    else:
+      msg = "Invalid attribute %s for color transformer %s. ColorTransformer.mode should be set to \"transform\" or \"reverse transform\"." % (self.mode, self)
+      self.logs[-1] += self.formatter.error(msg)
+      raise AttributeError(msg)
+
+    return self
+  
+
 class Spliter(Pipe):
   """
   Spliter splits each channels of image to tiles.
@@ -117,7 +211,7 @@ class LevelShifter(Pipe):
     name: str, optional
       Name of the spliter.
     mode: str, optional
-      Mode of the level shifter, must in ["shift", "recover"].
+      Mode of the level shifter, must in ["shift", "reverse shift"].
     depth: int, optional
       Depth of image.
     """
@@ -138,13 +232,13 @@ class LevelShifter(Pipe):
       for tile in X:
         tile = tile - 2 ** (self.depth - 1)
         tiles.append(tile)
-    elif self.mode == "recover":
+    elif self.mode == "reverse shift":
       self.logs[-1] += self.formatter.message("Recover image from {}-bit DC level shifting.".format(self.depth))
       for tile in X:
         tile = tile + 2 ** (self.depth - 1)
         tiles.append(tile)
     else:
-      msg = "Invalid attribute %s for level shifter %s. LevelShifter.mode should be set to \"shift\" or \"recover\"." % (self.mode, self)
+      msg = "Invalid attribute %s for level shifter %s. LevelShifter.mode should be set to \"shift\" or \"reverse shift\"." % (self.mode, self)
       self.logs[-1] += self.formatter.error(msg)
       raise AttributeError(msg)
 
@@ -170,7 +264,7 @@ class Normalizer(Pipe):
     name: str, optional
       Name of the spliter.
     mode: str, optional
-      Mode of the level shifter, must in ["normalize", "recover"].
+      Mode of the level shifter, must in ["normalize", "denormalize"].
     depth: int, optional
       Depth of image.
     """
@@ -189,12 +283,12 @@ class Normalizer(Pipe):
     if self.mode == "normalize":
       self.logs[-1] += self.formatter.message("Praticing {}-bit normalizing.".format(self.depth))
       for tile in X:
-        tile = tile / (2 ** self.depth)
+        tile = tile / (2 ** self.depth - 1)
         tiles.append(tile)
-    elif self.mode == "recover":
+    elif self.mode == "denormalize":
       self.logs[-1] += self.formatter.message("Praticing {}-bit denormalizing.".format(self.depth))
       for tile in X:
-        tile = tile * (2 ** self.depth)
+        tile = tile * (2 ** self.depth - 1)
         tiles.append(tile)
     else:
       msg = "Invalid attribute %s for noarmlizer %s. Normalizer.mode should be set to \"normalize\" or \"recover\"." % (self.mode, self)
