67bcaab42b1897390bc4ef5b0fc9ba50a9d7abf6
fields1631 <15218361552@163.com>
2020-11-12 20:51:03 +0800

    File for quantifier

--- fpeg/utils/quantify.py
+++ fpeg/utils/quantify.py
@@ -5,3 +5,21 @@ from ..config import read_config
 
 
 config = read_config()
+
+
+class Quantizer(Pipe):
+  """
+  Quantizer
+  """
+  def __init__(self,
+               name="Quantizer",
+               mode="quantify",
+               reversible=True):
+    """
+    
+    """
+    super().__init__()
+    
+    self.name = name
+    self.mode = mode
+    self.reversible = reversible
