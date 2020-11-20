c288cbe9e82475969e512fea936c1d79d7d9a794
fields1631 <15218361552@163.com>
2020-11-20 13:23:43 +0800

    update test

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 from fpeg.pipeline import Pipeline
-from fpeg.codec import HuffmanCodec
-from fpeg.utils.process import *
-from fpeg.utils.io import Reader, Writer
+from fpeg.codec import EBCOTCodec
+from fpeg.transormer import DWTransformer
+from fpeg.utils import *
 
 
 if __name__ == "__main__":
@@ -11,12 +11,9 @@ if __name__ == "__main__":
                       ("normalizer0", Normalizer()),
                       ("color transformer0", ColorTransformer()),
                       ("spliter0", Spliter()),
-                      ("codec0", HuffmanCodec()),
-                      ("spliter1", Spliter()),
-                      ("color transformer1", ColorTransformer()),
-                      ("normalizer1", Normalizer()),
-                      ("level shifter1", LevelShifter()),
-                      ("writer0", Writer())
+                      ("dw transformer0", DWTransformer()),
+                      ("quantizer0", Quantizer())
+                      ("codec0", EBCOTCodec()),
                       ],
                       params={
                       "reader0": {"flag": "color"},
@@ -24,12 +21,8 @@ if __name__ == "__main__":
                       "normalizer0": {"depth": 8},
                       "color transformer0": {"mode": "transform", "lossy": True},
                       "spliter0": {"tile_shape": (256, 256)},
-                      "codec0": {"mode": "encode", "accelerated": False, "use_lut": False},
-                      "spliter1": {"mode": "recover", "block_shape": (4, 5)},
-                      "color transformer1": {"mode": "reverse transform", "lossy": True},
-                      "normalizer1": {"mode": "denormalize", "depth": 8},
-                      "level shifter1": {"mode": "reverse shift", "depth": 8},
-                      "writer0": {"path": r"out/mudrock.jpg"}
+                      "dw transformer0": {"mode": "forward", "lossy": True}
+                      "codec0": {"mode": "encode", "accelerated": False},
                       })
 
   path = r"in/rosmontis.jpg"
