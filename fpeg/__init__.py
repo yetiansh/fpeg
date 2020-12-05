68dc03788e14a45336d2fc01f768a7e83d23986a
fields1631 <15218361552@163.com>
2020-12-05 22:47:58 +0800

    Update init

--- fpeg/__init__.py
+++ fpeg/__init__.py
@@ -1,4 +1,4 @@
-from .fpeg import fpeg
-from .jpeg import jpeg2000
+from .fpeg import fpeg_compress, fpeg_decompress
+from .jpeg import jpeg2000_compress, jpeg2000_decompress
 from .config import Config
 from .base import Pipe
